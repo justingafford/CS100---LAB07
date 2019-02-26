4363d9bdbdb64fa4b51afc5473d99d4515ba0ae4
brrcrites <brrcrites@gmail.com>
2019-02-26 14:25:39 -0800

    Add base factory code, update README

---
+++ base_factory.hpp
@@ -0,0 +1,14 @@
+#ifndef __BASE_FACTORY_HPP__
+#define __BASE_FACTORY_HPP__
+
+class BaseFactory {
+    public:
+        /* Constructor */
+        BaseFactory() {}
+
+        /* Pure Virtual Production Function */
+        virtual Op* createOp(double value) = 0;
+        virtual Rand* createRand() = 0;
+};
+
+#endif //__BASE_FACTORY_HPP__
