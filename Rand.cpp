e5993d8f7d865f10d1a0b3bc37d2a07289cb8fef
vtsai006 <49217659+vtsai006@users.noreply.github.com>
2019-05-17 16:02:08 -0700

    Add files via upload

---
+++ Rand.cpp
@@ -0,0 +1,11 @@
+#include "Rand.hpp"
+#include <string>
+#include <ctime>
+#include <ctime>
+using namespace std;
+
+
+        Rand::Rand() {num = rand() % 100;};
+        double Rand::evaluate() {return num;}
+        string Rand::stringify() {return to_string(num);}
+
