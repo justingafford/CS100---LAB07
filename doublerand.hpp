4b8c731980d360ffdee21c626a9a323f721111e2
Justin Gafford <jgaff001@ucr.edu>
2019-05-23 16:01:53 -0700

    Update doublerand.hpp

--- doublerand.hpp
+++ doublerand.hpp
@@ -1,1 +1,24 @@
+#ifndef DOUBLERAND_H
+#define DOUBLERAND_H
+#include "base_factory.hpp"
+#include "Rand.hpp"
+#include <sstream>
 
+
+class DoubleRand : public Rand
+{
+    protected:
+        double num;
+    public:
+        DoubleRand() { num = rand % 100; };
+        double evaluate() {};
+        string stringify() {
+            ostringstream obj;
+            obj << std::scientific;
+            obj << num;
+            string i = obj.str();
+            return i;
+        };      
+};
+
+#endif
