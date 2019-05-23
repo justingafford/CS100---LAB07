4315d8bd9e7df2b6abb7c6b78b52325bfbfbf21f
Justin Gafford <jgaff001@ucr.edu>
2019-05-23 15:53:47 -0700

    Update doubleop.hpp

--- doubleop.hpp
+++ doubleop.hpp
@@ -1,1 +1,25 @@
+#ifndef DOUBLEOP_H
+#define SCIOP_H
+#include "base_factory.hpp"
+#include "op.hpp"
+#include <sstream>
 
+
+class SciOp : public Op
+{
+    protected:
+        double num;
+    public:
+        SciOp() { num = 0; };
+        SciOp(double n) { num = n; };
+        double evaluate() {};
+        string stringify() {
+            ostringstream obj;
+            obj << std::scientific;
+            obj << num;
+            string i = obj.str();
+            return to_string(i);
+        };      
+};
+
+#endif
