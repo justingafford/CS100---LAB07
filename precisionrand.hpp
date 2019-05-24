d0dd02d702fcd7ef2088b58f8949665e39d05e3c
Justin Gafford <jgaff001@ucr.edu>
2019-05-23 17:08:25 -0700

    Update precisionrand.hpp

--- precisionrand.hpp
+++ precisionrand.hpp
@@ -1,1 +1,24 @@
+#ifndef PRECISIONRAND_H
+#define PRECISIONRAND_H
+#include "base_factory.hpp"
+#include "Rand.hpp"
+#include <sstream>
 
+
+class PrecisionRand : public Rand
+{
+    protected:
+        double num;
+    public:
+        PrecisonRand() { num = rand % 100; };
+        double evaluate() {};
+        string stringify() {
+            ostringstream obj;
+            obj << std::fixed;
+            obj << num;
+            string i = obj.str();
+            return i;
+        };      
+};
+
+#endif
