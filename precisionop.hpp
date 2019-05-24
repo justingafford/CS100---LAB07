3b137a459bf3504025e5de0e3697097aa70421eb
Justin Gafford <jgaff001@ucr.edu>
2019-05-23 17:07:32 -0700

    Update precisionop.hpp

--- precisionop.hpp
+++ precisionop.hpp
@@ -1,1 +1,25 @@
-#include Op.hpp
+#ifndef PRECISIONOP_H
+#define PRECISIONOP_H
+#include "base_factory.hpp"
+#include "Op.hpp"
+#include <sstream>
+
+
+class PrecisionOp : public Op
+{
+    protected:
+        double num;
+    public:
+        PrecisionOp() { num = 0; };
+        PrecisonOp(double n) { num = n; };
+        double evaluate() {};
+        string stringify() {
+            ostringstream obj;
+            obj << std::fixed;
+            obj << num;
+            string i = obj.str();
+            return i;
+        };      
+};
+
+#endif
